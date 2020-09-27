1f893669f137464d0f1b49d4a0ead58cdec56517
suryanshsaraswat01 <46866579+suryanshsaraswat01@users.noreply.github.com>
2020-09-27 13:14:24 +0530

    jai mata di

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { timeStamp } from 'console';
-import { map } from 'rxjs/Operator';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
